712b9b18b63c3f16fc66d66bd56a65d0024ba0aa
ismail <coderismail22@gmail.com>
2024-08-31 15:20:41 +0600

    add scrolltoptop component for viewport scroll issues

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -2,14 +2,16 @@ import { Outlet } from "react-router-dom";
 import Header from "../Pages/Shared/Header/Header";
 import Footer from "../Pages/Shared/Footer/Footer";
 import Navbar from "../Pages/Shared/Navbar/Navbar";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 
 const Root = () => {
   return (
     <div>
-      <Header></Header>
-      <Navbar></Navbar>
-      <Outlet></Outlet>
-      <Footer></Footer>
+      <ScrollToTop />
+      <Header />
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
   );
 };
