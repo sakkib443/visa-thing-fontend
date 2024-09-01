449f6af6a8fca81047ad51522ffa5f19ed47c8d9
ismail <coderismail22@gmail.com>
2024-09-01 13:10:36 +0600

    add icons to the user and admin dashboards

--- src/Pages/AdminDashboard/AdminDashboard/AdminDashboard.jsx
+++ src/Pages/AdminDashboard/AdminDashboard/AdminDashboard.jsx
@@ -1,6 +1,15 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { FaBars, FaHome } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { IoCloudDownloadOutline } from "react-icons/io5";
+import { FaCheckCircle } from "react-icons/fa";
+import { TbXboxXFilled } from "react-icons/tb";
+import { MdSpaceDashboard } from "react-icons/md";
+
+
+
+
 
 const AdminDashboard = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -15,16 +24,18 @@ const AdminDashboard = () => {
             <div className={`bg-blue-800 text-white w-64 p-4 lg:relative lg:w-64 ${isSidebarOpen ? "block" : "hidden"} lg:block`}>
                 <ul className="space-y-4">
                     <li>
-                        <Link to="admin-profile" className="block p-2 hover:bg-blue-600 rounded">Profile</Link>
+                        <Link to="admin-profile" className="flex items-center gap-2 p-2 hover:bg-blue-600 rounded"><CgProfile />
+                            Profile</Link>
                     </li>
                     <li>
-                        <Link to="admin-pending-applications" className="block p-2 hover:bg-blue-600 rounded">Pending Applications</Link>
+                        <Link to="admin-pending-applications" className="flex items-center gap-2 p-2 hover:bg-blue-600 rounded"><IoCloudDownloadOutline />
+                        Pending Applications</Link>
                     </li>
                     <li>
-                        <Link to="admin-approved-applications" className="block p-2 hover:bg-blue-600 rounded">Approved Applications</Link>
+                        <Link to="admin-approved-applications" className="flex items-center gap-2 p-2 hover:bg-blue-600 rounded"><FaCheckCircle />Approved Applications</Link>
                     </li>
                     <li>
-                        <Link to="admin-declined-applications" className="block p-2 hover:bg-blue-600 rounded">Declined Applications</Link>
+                        <Link to="admin-declined-applications" className="flex items-center gap-2 p-2 hover:bg-blue-600 rounded"><TbXboxXFilled className="size-5"/>Declined Applications</Link>
                     </li>
                     <li>
 
@@ -37,7 +48,7 @@ const AdminDashboard = () => {
             <div className="flex-1">
                 {/* Mobile Navbar */}
                 <div className="lg:hidden p-4 bg-blue-800 text-white flex justify-between">
-                    <h1 className="text-xl">Admin Dashboard</h1>
+                    <h1 className="text-xl flex items-center justify-center gap-2"><MdSpaceDashboard />Admin Dashboard</h1>
                     <FaBars className="cursor-pointer" onClick={toggleSidebar} />
                 </div>
 
--- src/Pages/UserDashboard/UserDashboard/UserDashboard.jsx
+++ src/Pages/UserDashboard/UserDashboard/UserDashboard.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { FaBars, FaHome } from "react-icons/fa";
+import { MdSpaceDashboard } from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
+import { IoCloudDownloadOutline } from "react-icons/io5";
+import { FaCheckCircle } from "react-icons/fa";
+import { TbXboxXFilled } from "react-icons/tb";
+
 
 const UserDashboard = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -15,16 +21,16 @@ const UserDashboard = () => {
             <div className={`bg-blue-800 text-white w-64 p-4 lg:relative lg:w-64 ${isSidebarOpen ? "block" : "hidden"} lg:block`}>
                 <ul className="space-y-4">
                     <li>
-                        <Link to="user-profile" className="block p-2 hover:bg-blue-600 rounded">Profile</Link>
+                        <Link to="user-profile" className="flex items-center gap-2 p-2 hover:bg-blue-600 rounded"><CgProfile className="size-5" />Profile</Link>
                     </li>
                     <li>
-                        <Link to="user-pending-applications" className="block p-2 hover:bg-blue-600 rounded">Pending Applications</Link>
+                        <Link to="user-pending-applications" className="flex items-center gap-2 p-2 hover:bg-blue-600 rounded"><IoCloudDownloadOutline className="size-5" />Pending Applications</Link>
                     </li>
                     <li>
-                        <Link to="user-approved-applications" className="block p-2 hover:bg-blue-600 rounded">Approved Applications</Link>
+                        <Link to="user-approved-applications" className="flex items-center gap-2 p-2 hover:bg-blue-600 rounded"><FaCheckCircle className="size-5" />Approved Applications</Link>
                     </li>
                     <li>
-                        <Link to="user-declined-applications" className="block p-2 hover:bg-blue-600 rounded">Declined Applications</Link>
+                        <Link to="user-declined-applications" className="flex items-center gap-2 p-2 hover:bg-blue-600 rounded"><TbXboxXFilled className="size-5" />Declined Applications</Link>
                     </li>
                     <li>
 
@@ -37,7 +43,7 @@ const UserDashboard = () => {
             <div className="flex-1">
                 {/* Mobile Navbar */}
                 <div className="lg:hidden p-4 bg-blue-800 text-white flex justify-between">
-                    <h1 className="text-xl">User Dashboard</h1>
+                    <h1 className="text-xl flex items-center gap-2"><MdSpaceDashboard />User Dashboard</h1>
                     <FaBars className="cursor-pointer" onClick={toggleSidebar} />
                 </div>
 
